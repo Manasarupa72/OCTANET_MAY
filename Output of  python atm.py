d1b179c6514843f6e79df5db4d864c653a8a18b8
Manasarupa72 <145731727+Manasarupa72@users.noreply.github.com>
2024-05-21 09:33:39 +0530

    Create Output of  python atm.py

---
+++ Output of  python atm.py
@@ -0,0 +1,33 @@
+Please insert your Card
+Enter your atm pin 1234
+
+          1 == balance
+          2 == withdraw
+          3 == deposite
+          4 == exit 
+Please enter your choice 1 
+Your current balance is 5000
+
+          1 == balance
+          2 == withdraw
+          3 == deposite
+          4 == exit 
+Please enter your choice 2
+Please enter withdrawal amount 400
+400 is deducted from your account
+your current balance is 4600
+
+          1 == balance
+          2 == withdraw
+          3 == deposite
+          4 == exit
+Please enter your choice 3
+Please enter deposite_amount 200
+200 is credited to your account
+Your updated balance is 4800
+
+          1 == balance
+          2 == withdraw
+          3 == deposite
+          4 == exit
+Please enter your choice 4
